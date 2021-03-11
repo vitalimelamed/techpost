89f531feee9408fd37c7309b0be4fe0348a8aaf4
Vitali Melamed <49156662+vitalimelamed@users.noreply.github.com>
2021-03-11 17:56:41 +1100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,12 +7,6 @@ const navbarToggle = document.querySelector(".navbar-toggle");
 navbarToggle.addEventListener("click", () => {
   document.body.classList.toggle("navbar-open");
 });
-// Jquery prevent scroll while navbar overlay open
-$(".navbar-menu").on("scroll touchmove mousewheel", function (e) {
-  e.preventDefault();
-  e.stopPropagation();
-  return false;
-});
 // Jquery top button fade in and fade out on scroll
 $(window).scroll(function () {
   if ($(this).scrollTop() > 300) {
