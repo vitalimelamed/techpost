4534703e50f545ec43f9455651457d90401e57a4
Vitali Melamed <49156662+vitalimelamed@users.noreply.github.com>
2021-03-30 23:22:24 +1100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,11 @@
-// Jquery change navbar background color on scroll
-$(window).scroll(function () {
-    $(".navbar").toggleClass("active", $(this).scrollTop() > 10);
-  });
-// Listen for a click event on navbar toggle
 const navbarToggle = document.querySelector(".navbar-toggle");
-navbarToggle.addEventListener("click", () => {
-  document.body.classList.toggle("navbar-open");
+const navbarList = document.querySelector(".navbar-list");
+const navbarLink = document.querySelectorAll(".navbar-link");
+  navbarToggle.addEventListener("click", () => {
+    navbarList.classList.toggle("navbar-open");
 });
-// Jquery top button fade in and fade out on scroll
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 300) {
-    $(".top").fadeIn();
-  } else {
-    $(".top").fadeOut();
-  }
+  navbarLink.forEach(link => {
+    link.addEventListener('click', () => {
+      navbarList.classList.remove('navbar-open');
+  });
 });
